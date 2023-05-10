070ea8b68fdbde3eb7250ae409c59fc5dee56e65
Chai Pin Zheng <chaipinzheng@gmail.com>
2023-05-11 01:05:03 +0800

    Optimising the accordion by reducing direct dom manipulation

--- src/accordion.js
+++ src/accordion.js
@@ -1,3 +1,10 @@
+import sanitize from 'sanitize-html';
+
+// Sanitize the HTML content of the accordion
+export function sanitizeHtml(html) {
+  return sanitize(html);
+}
+
 function handleAccordionClick(id) {
   const accordion = document.querySelector(`[data-id="${id}"]`);
   const header = accordion.querySelector('a.sgds-accordion-header');
@@ -15,50 +22,53 @@ export function install(hook, vm) {
   hook.doneEach(function () {
     try {
       const accordions = document.querySelectorAll('details > summary');
-      accordions.forEach((accordion) => {
-        // Assign a unique id to the accordion to be used for the onclick event
-        const assignedUniqueAccordionID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+      accordions.forEach((accordion, key) => {
         // Parent element of the accordion
         const sgdsAccordion = document.createElement('div');
-        sgdsAccordion.classList.add('sgds-accordion', 'margin--bottom');
-        sgdsAccordion.setAttribute('data-id', assignedUniqueAccordionID);
-        sgdsAccordion.onclick = function () { handleAccordionClick(assignedUniqueAccordionID); };
+        sgdsAccordion.classList.add('sgds-accordion', 'margin--bottom--sm');
+        sgdsAccordion.setAttribute('data-id', key);
 
         // First child of the accordion
         const accordionHeader = document.createElement('a');
         accordionHeader.classList.add('sgds-accordion-header', 'padding--top', 'padding--bottom', 'has-text-dark', 'has-background-white');
         accordionHeader.setAttribute('role', 'button');
         accordionHeader.setAttribute('aria-expanded', 'false');
-        accordionHeader.innerHTML = `<div>${accordion.innerHTML}</div><i class="sgds-icon sgds-icon-chevron-up"></i>`;
+        accordionHeader.innerHTML = `<div>${sanitizeHtml(accordion.innerHTML)}</div><i class="sgds-icon sgds-icon-chevron-up"></i>`;
+        accordionHeader.onclick = function () { handleAccordionClick(key); };
 
         // Second child of the accordion
         const accordionBody = document.createElement('div');
         accordionBody.classList.add('sgds-accordion-body');
         // We are currently in the summary element, so we need to get the parent element before we can extract the HTML content of the details element apart from the summary element
         const accordionParent = accordion.parentNode;
+        // Instead of appending the elements directly to the DOM, we append them to a document fragment and then append the fragment to the DOM. This can improve performance by reducing the number of DOM operations.
+        const fragment = document.createDocumentFragment();
         // Iterate over the child nodes of the details element
         for (let i = 0; i < accordionParent.childNodes.length; i++) {
           const childNode = accordionParent.childNodes[i];
 
-          // if its an html element or text node
-          if ((childNode.tagName && childNode.tagName.toLowerCase() !== 'summary') || childNode.nodeType === Node.TEXT_NODE) {
-            // Filter out br elements
-            if (childNode.tagName && childNode.tagName.toLowerCase() === 'br' && i !== 0 && i !== accordionParent.childNodes.length - 1) {
-              continue;
-            }
+          // Skip the summary element by not adding it to the fragment
+          if ((childNode.tagName && childNode.tagName.toLowerCase() === 'summary')) {
+            continue;
+          }
 
-            if (childNode.nodeType === Node.ELEMENT_NODE) {
-              // Skip the summary element
-              accordionBody.innerHTML += childNode.outerHTML;
-            }
+          // Skip the first and last br elements by not adding them to the fragment
+          if (childNode.tagName && childNode.tagName.toLowerCase() === 'br' && i !== 0 && i !== accordionParent.childNodes.length - 1) {
+            continue;
+          }
 
-            if (childNode.nodeType === Node.TEXT_NODE && childNode.textContent.trim() !== '') {
-              // Skip the empty text nodes
-              accordionBody.innerHTML += childNode.textContent;
-            }
+          if (childNode.nodeType === Node.ELEMENT_NODE) {
+            fragment.appendChild(childNode.cloneNode(true));
+          }
+
+          if (childNode.nodeType === Node.TEXT_NODE && childNode.textContent.trim() !== '') {
+            fragment.appendChild(document.createTextNode(childNode.textContent));
           }
         }
 
+        // Add the content of the details element to the accordion body
+        accordionBody.appendChild(fragment);
+        console.log(accordionBody);
         // Add the header and body to the accordion
         sgdsAccordion.appendChild(accordionHeader);
         sgdsAccordion.appendChild(accordionBody);
